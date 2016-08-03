b6f3618c837a0bca9ae7415a474215b2d7e9f1e8
Y'Eli Buonya <yeli.e.buonya@gmail.com>
2016-08-03 01:48:08 +0000

    add first_class.rb

---
+++ vwc_digimon/first_class.rb
@@ -0,0 +1,23 @@
+# super class
+class Human
+    def name
+        puts "Y'Eli"
+        puts "Buonya"
+    end
+    
+    def gender
+        puts "Dude"
+    end
+    
+    def service
+       puts "US ARMY INFANTRY" 
+    end
+end
+
+# sub class
+class You < Human
+end
+
+class Me < Human
+end
+
