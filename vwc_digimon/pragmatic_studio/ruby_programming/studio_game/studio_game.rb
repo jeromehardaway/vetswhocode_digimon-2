421eb46c26b2cc5c96b86622bf880239ecf1a5f9
Y'Eli Buonya <yeli.e.buonya@gmail.com>
2016-08-04 16:10:02 +0000

    add files

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -0,0 +1,11 @@
+greeting = "Welcome!" # assign welcome message to variable greeting
+
+# prints to the screen 3 times in a loop using the upcase method on greeting
+
+3.times do             
+  puts greeting.upcase
+end
+
+# print current time
+
+puts Time.now
