1d14ca3f6e2d221ef19c4d533e083a7249e3c750
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-10 15:36:16 -0700

    complete module lesson

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   attr_reader :title
@@ -14,22 +15,14 @@ class Game
   end
 
   def play
-    puts "There are #{@players.size} players in #{@title}: "
+    puts "There are #{@players.size} players in : "
+
     @players.each do |player|
       puts player
     end
 
     @players.each do |player|
-      die = Die.new
-      number_rolled = die.roll
-      case number_rolled
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player.name} was skipped"
-      else
-        player.w00t
-      end
+      GameTurn.take_turn(player)
       puts player
     end
   end
