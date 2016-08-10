2adbd08cb47baf447d2f7b616913ec3d6c8cdc53
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-10 13:56:32 -0700

    complete part 2 TDD

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game.rb
@@ -1,26 +1,33 @@
 require_relative 'player'
+require_relative 'die'
 
 class Game
   attr_reader :title
-  
+
   def initialize(title)
     @title = title
     @players = []
   end
-  
+
   def add_player(player)
     @players << player
   end
-  
+
   def play
     puts "There are #{@players.size} players in #{@title}: "
     @players.each do |player|
       puts player
     end
+
     @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
+      die = Die.new
+      number_rolled = die.roll
+
+      if number_rolled < 5
+        puts "#{player.name} was skipped"
+      else
+        player.w00t
+      end
       puts player
     end
   end
@@ -32,7 +39,7 @@ if __FILE__ == $0
   player_1 = Player.new("alvin")
   player_2 = Player.new("simon")
   player_3 = Player.new("theodore")
-  
+
   chipmunks = Game.new("chipmunks")
   chipmunks.add_player(player_1)
   chipmunks.add_player(player_2)
---
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'game'
+
+describe Game do
+
+  before do
+    @game = Game.new("Knuckleheads")
+
+    @initial_health = 100
+    @player = Player.new("moe", @initial_health)
+
+    @game.add_player(@player)
+  end
+
+  it "w00ts the player if a high number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(5)
+  end
+
+  it "skips the player if a medium number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(3)
+
+    @game.play
+
+    expect(@player.health).to eq(@initial_health)
+  end
+end
