e992ebf4c3ad70f4c009fd6c622c3f79673bfa40
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 15:30:32 -0700

    complete classes

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -1,15 +1,41 @@
-require_relative 'game'
-require_relative 'player'
+class Player
+  def initialize(name, health=100) #set default health to 100
+    @name = name.capitalize
+    @health = health
+  end
 
-# create player objects
+  def to_s # prints a hello message from Player
+    "I'm #{@name} with a health of #{@health}"
+  end
+
+  def blam
+    @health -= 10 # decreases a players health by 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15 # increases a players health by 10
+    puts "#{@name} got w00ted!"
+  end
+end
+
+
 player1 = Player.new("moe")
-player2 = Player.new("larry", 60)
+player2 = Player.new("larry", 65)
 player3 = Player.new("curly", 125)
 
-# create game
-knuckleheads = Game.new("Knuckleheads")
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
-knuckleheads.play(3)
-knuckleheads.print_stats
+puts player1
+puts player2
+puts player3
+
+player1.blam
+player2.blam
+player3.blam
+
+puts player1
+puts player2
+puts player3
+
+player1.w00t
+player2.w00t
+player3.w00t
