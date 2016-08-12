0ded1f6d3f026add9f9c5e0735982e43173320cc
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-11 19:21:33 -0700

    add files for Symbols and Structs

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/treasure_trove.rb
@@ -0,0 +1,16 @@
+# create new Struct
+Treasure = Struct.new(:name, :points)
+
+# define a TreasureTrove Module
+module TreasureTrove
+
+  TREASURES = [
+    Treasure.new(:pie, 5),
+    Treasure.new(:bottle, 25),
+    Treasure.new(:hammer, 50),
+    Treasure.new(:skillet, 100),
+    Treasure.new(:broomstick, 200),
+    Treasure.new(:crowbar, 400)
+  ]
+
+end
