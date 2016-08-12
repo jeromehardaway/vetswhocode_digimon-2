1793fa4f7fc7a6434ccb5efcd42f32129a7936e0
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 16:20:42 -0700

    complete arrays

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -24,6 +24,10 @@ class Player
     @health += 15 # increases a players health by 10
     puts "#{@name} got w00ted!"
   end
+
+  def name=(name)
+    @name = name.capitalize
+  end
 end
 
 # create player objects
@@ -31,6 +35,25 @@ player1 = Player.new("moe")
 player2 = Player.new("larry", 65)
 player3 = Player.new("curly", 125)
 
-puts player1.health # use health attribute to display player health
-player2.name = "Lawrence" # use name attribute to change player name
-puts player2
+# create an array of playerss
+players = [player1, player2, player3]
+
+# iterate through each player and print details
+puts "There are #{players.size} players in the game"
+players.each { |player| puts player }
+
+# print out the health of each player using blocks
+players.each { |player| puts "#{player.name} #{player.health}" }
+
+# itereate through the players and blam, w000t, and print details
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+# remove curly and add shemp
+players.pop
+player4 = Player.new("shemp", 90)
+players.push(player4)
