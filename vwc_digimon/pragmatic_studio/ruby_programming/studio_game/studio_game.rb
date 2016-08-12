cd45a83ec99c2f1eb29349d0eb8619c41dce0a34
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 15:53:54 -0700

    complete attributes

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -1,11 +1,18 @@
 class Player
+  attr_reader :health # use attributes can be accessed outside of class
+  attr_accessor :name # read and write attribute
+
   def initialize(name, health=100) #set default health to 100
     @name = name.capitalize
     @health = health
   end
 
   def to_s # prints a hello message from Player
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}" #called method score and used in string interpolation
+  end
+
+  def score # virtual accessor method to determine score
+    @health + @name.length
   end
 
   def blam
@@ -19,23 +26,11 @@ class Player
   end
 end
 
-
+# create player objects
 player1 = Player.new("moe")
 player2 = Player.new("larry", 65)
 player3 = Player.new("curly", 125)
 
-puts player1
+puts player1.health # use health attribute to display player health
+player2.name = "Lawrence" # use name attribute to change player name
 puts player2
-puts player3
-
-player1.blam
-player2.blam
-player3.blam
-
-puts player1
-puts player2
-puts player3
-
-player1.w00t
-player2.w00t
-player3.w00t
