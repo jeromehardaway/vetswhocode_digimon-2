97d0601780a6798d7d3f60800f1a6f152ce2fce8
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 18:04:35 -0700

    add new features

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/fundraising_program.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/fundraising_program.rb
@@ -26,15 +26,10 @@ class Project
   end
 end
 
-# create project objects
-project1 = Project.new("Drone", 0, 5000)
-project2 = Project.new("House", 0, 100_000_000)
-project3 = Project.new("Vacation", 0, 7000)
-
-# create project array and add projects
-projects = [project1, project2, project3]
 
 =begin
+# create project array and add projects
+projects = [project1, project2, project3]
 
 # iterate through each project and print details
 projects.each do |project|
@@ -53,7 +48,7 @@ projects.each do |project|
   puts project
 end
 
-=end
+
 
 # remove one project from your list, add a new project, and print out an updated list of project information.
 projects.pop
@@ -61,3 +56,36 @@ project4 = Project.new("Food", 50, 70)
 projects.push(project4)
 
 puts projects
+=end
+
+class Startup
+  attr_reader :projects_title # create readable attributeto access outside of class
+  def initialize(projects_title)
+    @projects_title = projects_title.capitalize
+    @projects = Array.new
+  end
+  # define a method that adds a project to Projects array
+  def add_project(project_name)
+    @projects << project_name # add projects to array
+  end
+
+  def display_project # create instance method to display current projects
+    puts "There are #{@projects.size} projects in #{@projects_title}"
+    @projects.each { |project| puts project }
+    # Iterate through your projects and print out the target funding amount of each project.
+    @projects.each do |project|
+     puts "Project: #{project.project_name} amount needed: #{project.target_amount}"
+    end
+  end
+end
+
+# create project objects
+project1 = Project.new("Drone", 0, 5000)
+project2 = Project.new("House", 0, 100_000_000)
+project3 = Project.new("Vacation", 0, 7000)
+
+projects = Startup.new("current projects")
+projects.add_project(project1)
+projects.add_project(project2)
+projects.add_project(project3)
+projects.display_project
