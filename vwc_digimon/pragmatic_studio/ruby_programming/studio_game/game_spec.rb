ef6b0dab7a91bb2780583dd510f67f03bd9b21cb
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-13 09:39:13 -0700

    add tests

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game_spec.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/game_spec.rb
@@ -1,9 +1,9 @@
 require_relative 'game'
-
+# Rspec test for game.rb
 describe Game do
-
   before do
-    @game = Game.new("Knuckleheads")
+    $stdout = StringIO.new
+    @game = Game.new("knuckleheads")
 
     @initial_health = 100
     @player = Player.new("moe", @initial_health)
@@ -11,74 +11,28 @@ describe Game do
     @game.add_player(@player)
   end
 
-  it "has a title" do
-    @game.title.should == "Knuckleheads"
-  end
-
   it "w00ts the player if a high number is rolled" do
     Die.any_instance.stub(:roll).and_return(5)
 
-    @game.play(2)
+    @game.play
 
-    @player.health.should == @initial_health + (15 * 2)
+    @player.health.should == @initial_health + 15
   end
 
-  it "skips the player if a medium number is rolled" do
+  it "skips the player if a mediumnumber is rolled" do
     Die.any_instance.stub(:roll).and_return(3)
 
-    @game.play(2)
+    @game.play
 
     @player.health.should == @initial_health
   end
 
-  it "blams the player if a low number is rolled" do
+  it "blams the player when a low number is rolled" do
     Die.any_instance.stub(:roll).and_return(1)
 
-    @game.play(2)
+    @game.play
 
-    @player.health.should == @initial_health - (10 * 2)
+    @player.health.should == @initial_health - 10
   end
 
-  it "assigns a treasure for points during a player's turn" do
-    game = Game.new("Knuckleheads")
-    player = Player.new("moe")
-
-    game.add_player(player)
-
-    game.play(1)
-
-    player.points.should_not be_zero
-  end
-
-  it "computes total points as the sum of all player points" do
-    game = Game.new("Knuckleheads")
-
-    player1 = Player.new("moe")
-    player2 = Player.new("larry")
-
-    game.add_player(player1)
-    game.add_player(player2)
-
-    player1.found_treasure(Treasure.new(:hammer, 50))
-    player1.found_treasure(Treasure.new(:hammer, 50))
-    player2.found_treasure(Treasure.new(:crowbar, 400))
-
-    game.total_points.should == 500
-  end
-
-  it "computes total points as the sum of all player points" do
-    game = Game.new("Knuckleheads")
-
-    player1 = Player.new("moe")
-    player2 = Player.new("larry")
-
-    game.add_player(player1)
-    game.add_player(player2)
-
-    player1.found_treasure(Treasure.new(:hammer, 50))
-    player1.found_treasure(Treasure.new(:hammer, 50))
-    player2.found_treasure(Treasure.new(:crowbar, 400))
-
-    game.total_points.should == 500
-  end
 end
