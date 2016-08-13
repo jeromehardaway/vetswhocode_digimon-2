51eef35cd55eb8eaabbdc707eb9e776381769524
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 17:45:21 -0700

    complete objects interacting

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -30,17 +30,9 @@ class Player
   end
 end
 
-# create player objects
-player1 = Player.new("moe")
-player2 = Player.new("larry", 65)
-player3 = Player.new("curly", 125)
-
+=begin
 # create an array of playerss
 players = [player1, player2, player3]
-
-# iterate through each player and print details
-puts "There are #{players.size} players in the game"
-players.each { |player| puts player }
 
 # print out the health of each player using blocks
 players.each { |player| puts "#{player.name} #{player.health}" }
@@ -56,4 +48,57 @@ end
 # remove curly and add shemp
 players.pop
 player4 = Player.new("shemp", 90)
-players.push(player4)
+players << player4
+=end
+
+# create a new class game
+class Game
+  attr_reader :game_title # create a readable attribute to access outside class
+  def initialize(game_title) # initialize method that allows us to create games
+    @game_title = game_title.capitalize # capitalize title
+    @players = Array.new # create an empty array for players
+  end
+
+  def add_player(new_player) # define a method that adds players
+    @players << new_player
+  end
+
+  def play # create instance method to play a game
+    # iterate through each player and print details
+    puts "There are #{@players.size} players in #{@game_title}"
+    @players.each { |player| puts player }
+
+    # iterate through the players and blam, w000t, and print details
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
+end
+
+
+# create player objects
+player1 = Player.new("moe")
+player2 = Player.new("larry", 65)
+player3 = Player.new("curly", 125)
+
+# create new game object
+knuckleheads = Game.new("knuckleheads")
+# add players to game
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+# create another game object with a different set of players
+player4 = Player.new("alvin")
+player5 = Player.new("simon")
+player6 = Player.new("calvin")
+
+chipmunks = Game.new("chipmunks")
+chipmunks.add_player(player4)
+chipmunks.add_player(player5)
+chipmunks.add_player(player6)
+chipmunks.play
