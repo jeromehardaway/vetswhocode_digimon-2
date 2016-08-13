1cd593f83b41e75bf8797888485e7e34db203ea6
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-12 18:14:10 -0700

    update main program

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/studio_game.rb
@@ -1,34 +1,19 @@
-class Player
-  attr_reader :health # use attributes can be accessed outside of class
-  attr_accessor :name # read and write attribute
+require_relative 'player'
+require_relative 'game'
 
-  def initialize(name, health=100) #set default health to 100
-    @name = name.capitalize
-    @health = health
-  end
+# create player objects
+player1 = Player.new("moe")
+player2 = Player.new("larry", 65)
+player3 = Player.new("curly", 125)
 
-  def to_s # prints a hello message from Player
-    "I'm #{@name} with a health of #{@health} and a score of #{score}" #called method score and used in string interpolation
-  end
+# create new game object
+knuckleheads = Game.new("knuckleheads")
+# add players to game
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
 
-  def score # virtual accessor method to determine score
-    @health + @name.length
-  end
-
-  def blam
-    @health -= 10 # decreases a players health by 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15 # increases a players health by 10
-    puts "#{@name} got w00ted!"
-  end
-
-  def name=(name)
-    @name = name.capitalize
-  end
-end
 
 =begin
 # create an array of playerss
@@ -49,48 +34,6 @@ end
 players.pop
 player4 = Player.new("shemp", 90)
 players << player4
-=end
-
-# create a new class game
-class Game
-  attr_reader :game_title # create a readable attribute to access outside class
-  def initialize(game_title) # initialize method that allows us to create games
-    @game_title = game_title.capitalize # capitalize title
-    @players = Array.new # create an empty array for players
-  end
-
-  def add_player(new_player) # define a method that adds players
-    @players << new_player
-  end
-
-  def play # create instance method to play a game
-    # iterate through each player and print details
-    puts "There are #{@players.size} players in #{@game_title}"
-    @players.each { |player| puts player }
-
-    # iterate through the players and blam, w000t, and print details
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
-    end
-  end
-end
-
-
-# create player objects
-player1 = Player.new("moe")
-player2 = Player.new("larry", 65)
-player3 = Player.new("curly", 125)
-
-# create new game object
-knuckleheads = Game.new("knuckleheads")
-# add players to game
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
-knuckleheads.play
 
 # create another game object with a different set of players
 player4 = Player.new("alvin")
@@ -102,3 +45,5 @@ chipmunks.add_player(player4)
 chipmunks.add_player(player5)
 chipmunks.add_player(player6)
 chipmunks.play
+
+=end
