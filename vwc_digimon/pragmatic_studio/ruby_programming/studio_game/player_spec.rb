b2e552c2e0b1fc1a65b37ce365ce1cd42f982c55
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-13 11:51:44 -0700

    add test specs

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
@@ -53,4 +53,18 @@ describe Player do
       @player.should_not be_strong
     end
   end
+
+  context "in a collection of players" do
+  before do
+    @player1 = Player.new("moe", 100)
+    @player2 = Player.new("larry", 200)
+    @player3 = Player.new("curly", 300)
+
+    @players = [@player1, @player2, @player3]
+  end
+
+  it "is sorted by highest to lowest score" do
+    @players.sort.should == [@player3, @player2, @player1]
+  end
+end
 end
