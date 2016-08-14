feb4ba9bc6fd0ad6078676b2af07ecfe502afffe
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-13 18:49:33 -0700

    add test specs

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'player'
+require_relative 'treasure_trove'
 # Rspec tests for player.rb
 describe Player do
   # examples go here
@@ -65,6 +66,22 @@ describe Player do
 
   it "is sorted by highest to lowest score" do
     @players.sort.should == [@player3, @player2, @player1]
+  end
+
+  it "computes points as the sum of all treasure points" do
+    @player.points.should == 0
+
+    @player.found_treasure(Treasure.new(:hammer, 50))
+
+    @player.points.should == 50
+
+    @player.found_treasure(Treasure.new(:crowbar, 400))
+
+    @player.points.should == 450
+
+    @player.found_treasure(Treasure.new(:hammer, 50))
+
+    @player.points.should == 500
   end
 end
 end
