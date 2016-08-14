88f4e46d2f5dd3bf323ea1dd9286e12a21178910
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-13 19:53:12 -0700

    update to_s example

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player_spec.rb
@@ -21,7 +21,7 @@ describe Player do
     @player.found_treasure(Treasure.new(:hammer, 50))
     @player.found_treasure(Treasure.new(:hammer, 50))
 
-    @player.to_s.should == "I'm Larry with a health of 150 and a score of 250"
+    @player.to_s.should == "I'm Larry with health = 150, points = 100, and score = 250"
   end
 
   it "computes a score as the sum of its health and length of name" do
