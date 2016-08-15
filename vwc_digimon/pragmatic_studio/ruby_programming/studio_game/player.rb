95046770cd1579ceaf4b876ec58f33c16d1d2a0a
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-14 19:56:22 -0700

    add method from_csv

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/player.rb
@@ -54,6 +54,11 @@ class Player
       yield Treasure.new(name, points)
     end
   end
+
+  def self.from_csv(string)
+    name, health = string.split(',')
+    Player.new(name, Integer(health))
+  end
 end
 
 if __FILE__ == $0
