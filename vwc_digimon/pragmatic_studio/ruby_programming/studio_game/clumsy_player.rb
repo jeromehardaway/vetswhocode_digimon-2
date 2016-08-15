4dd1547d54fea7b3727cdd1899dcd239e0152e51
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-15 11:18:27 -0700

    make class ClumsyPlayer

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/clumsy_player.rb
@@ -0,0 +1,30 @@
+require_relative 'player'
+require_relative 'treasure_trove'
+# define a ClumsyPlayer class that inherits from player
+class ClumsyPlayer < Player
+
+  def found_treasure(treasure)
+    # set damaged_treasure to an instance of class Treasure that takes original treasure name and half the original points as parameters
+    damaged_treasure = Treasure.new(treasure.name, treasure.points / 2.0)
+    # pass the new instance back to the default @found_treasure method
+    super(damaged_treasure)
+  end
+end
+
+# example
+if __FILE__ == $0
+  clumsy = ClumsyPlayer.new("klutz")
+
+  hammer = Treasure.new(:hammer, 50)
+  clumsy.found_treasure(hammer)
+  clumsy.found_treasure(hammer)
+  clumsy.found_treasure(hammer)
+
+  crowbar = Treasure.new(:crowbar, 400)
+  clumsy.found_treasure(crowbar)
+
+  clumsy.each_found_treasure do |treasure|
+    puts "#{treasure.points} total #{treasure.name} points"
+  end
+  puts "#{clumsy.points} grand total points"
+end
