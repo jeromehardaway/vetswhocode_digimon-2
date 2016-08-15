b3b10feb8526850e8e6620292f4debad10edb174
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-15 10:46:43 -0700

    update file

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/clumsy_player_spec.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/clumsy_player_spec.rb
@@ -28,20 +28,3 @@ describe ClumsyPlayer do
     yielded.should == [Treasure.new(:hammer, 75), Treasure.new(:crowbar, 200)]
   end
 end
-
-if __FILE__ == $0
-  clumsy = ClumsyPlayer.new("klutz")
-
-  hammer = Treasure.new(:hammer, 50)
-  clumsy.found_treasure(hammer)
-  clumsy.found_treasure(hammer)
-  clumsy.found_treasure(hammer)
-
-  crowbar = Treasure.new(:crowbar, 400)
-  clumsy.found_treasure(crowbar)
-
-  clumsy.each_found_treasure do |treasure|
-    puts "#{treasure.points} total #{treasure.name} points"
-  end
-  puts "#{clumsy.points} grand total points"
-end
