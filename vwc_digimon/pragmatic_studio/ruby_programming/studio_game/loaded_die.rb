12dadafa4deccbe1f7025866897fb2323bc67b95
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-16 09:41:32 -0700

    create class LoadedDie

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/loaded_die.rb
@@ -0,0 +1,14 @@
+require_relative 'auditable'
+# this class creates a type of cheat
+class LoadedDie
+  include Auditable
+
+  attr_reader :number
+
+  def roll
+    numbers = [1, 1, 2, 5, 6, 6]
+    @number = numbers.sample
+    audit
+    @number
+  end
+end
