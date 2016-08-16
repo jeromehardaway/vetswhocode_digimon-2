6af5b85bccb8e544be6ec85e9b8024837e2ec002
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-16 09:42:32 -0700

    include audit module

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/dice.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/dice.rb
@@ -1,5 +1,9 @@
+require_relative 'auditable'
+
 # for generating a random number
 class Dice
+  include Auditable # include Module
+
   attr_reader :number
 
   def initialize # calls the roll method which sets @number to random number
@@ -8,5 +12,8 @@ class Dice
 
   def roll
     @number = rand(1..6)
+    audit # call audit method after number is rolled
+    @number # return the number as the last expression since the games expects
+            # roll to return a number
   end
 end
