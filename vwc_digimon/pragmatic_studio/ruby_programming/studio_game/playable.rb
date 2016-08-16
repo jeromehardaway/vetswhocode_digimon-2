97727a9715bbc7952f7afca38fa4f9c5126c9060
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-16 10:05:18 -0700

    update to use attributes instead of instance variables

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/playable.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/playable.rb
@@ -1,15 +1,17 @@
+require_relative 'player'
+
 module Playable
   def blam
-    @health -= 10 # decreases a players health by 10
-    puts "#{@name} got blammed!"
+    self.health -= 10 # decreases a players health by 10
+    puts "#{name} got blammed!"
   end
 
   def w00t
-    @health += 15 # increases a players health by 15
-    puts "#{@name} got w00ted!"
+    self.health += 15 # increases a players health by 15
+    puts "#{name} got w00ted!"
   end
 
   def strong?
-    @health > 100
+    health > 100
   end
 end
