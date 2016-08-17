0c44dd2c9848e88a2772410c423fca8de0c03ef0
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-17 11:42:51 -0700

    add namespaces

--- vwc_digimon/pragmatic_studio/ruby_programming/studio_game/bin/studio_game.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/studio_game/bin/studio_game.rb
@@ -1,21 +1,22 @@
-require_relative 'player'
-require_relative 'game'
-require_relative 'clumsy_player'
-require_relative 'berserk_player'
+require_relative '../lib/studio_game/player'
+require_relative '../lib/studio_game/game'
+require_relative '../lib/studio_game/clumsy_player'
+require_relative '../lib/studio_game/berserk_player'
 
 # create player objects
-player1 = Player.new("moe")
-player2 = Player.new("larry", 65)
-player3 = Player.new("curly", 125)
-klutz = ClumsyPlayer.new("klutz", 105, 1)
-berserker = BerserkPlayer.new("berserk", 50)
+player1 = StudioGame::Player.new("moe")
+player2 = StudioGame::Player.new("larry", 65)
+player3 = StudioGame::Player.new("curly", 125)
+klutz = StudioGame::ClumsyPlayer.new("klutz", 105, 1)
+berserker = StudioGame::BerserkPlayer.new("berserk", 50)
+clumsy = StudioGame::ClumsyPlayer.new("klutz", 105, 3)
 
-clumsy = ClumsyPlayer.new("klutz", 105, 3)
+default_player_file = File.join(File.dirname(__FILE__), 'players.csv')
 
 # create new game object
 knuckleheads = StudioGame::Game.new("knuckleheads")
 #ARGV loads csv file from command line if given
-knuckleheads.load_players(ARGV.shift || "players.csv")
+knuckleheads.load_players(ARGV.shift || default_player_file)
 
 knuckleheads.add_player(klutz)
 knuckleheads.add_player(berserker)
