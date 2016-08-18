0a960c91fe69fed69e8600e0a4ce71b5bb9ecaf0
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-18 08:54:48 -0700

    add practice exercises

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/block_basics.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/block_basics.rb
@@ -1,1 +1,24 @@
-puts "Let's Master some damn Ruby blocks!"
+# use times method to with a block to print the following workout 5 times
+=begin
+5.times do
+  puts "situp"
+  puts "pushup"
+  puts "chinup"
+end
+=end
+
+# use block paramters
+=begin
+5.times do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
+=end
+
+# use .upto method
+1.upto(5) do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
