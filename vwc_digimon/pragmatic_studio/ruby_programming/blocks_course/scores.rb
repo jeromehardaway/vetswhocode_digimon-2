d9bc081cf76e2c039d0672569a41ce14ad93e50d
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-19 14:49:41 -0700

    add exercises

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/scores.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/scores.rb
@@ -15,3 +15,30 @@ p less_than_70
 # find the first score that is less than 70
 first_less_than_70 = scores.detect { |score| score < 70 }
 p first_less_than_70
+
+# permanently change the scores array to only include scores greater than 70
+scores.select! { |high_score| high_score > 80 }
+p scores
+
+# permanently kick out all the even scores
+scores.reject! { |score| score.even? }
+p
+
+# map the following scores array into a new array called scores_doubled that contains all the scores multiplied by 2 (double points!).
+scores = [83, 71, 92, 64, 98, 87, 75, 69]
+
+scores_doubled = scores.map { |score| score * 2 }
+p scores_doubled
+
+# Use the reduce method to efficiently sum up all the scores in the array and print the total
+total = scores_doubled.reduce(0) { |sum, score| sum + score }
+puts "Total score: #{total}"
+
+# alternate method
+total = scores_doubled.reduce(0, :+)
+puts "Total score: #{total}"
+
+# partition the scores into two arrays called evens and odds and print them out
+evens, odds  = scores.partition { |score| score.even? }
+puts "Even scores: #{evens}"
+puts "Odd scores: #{odds}"
