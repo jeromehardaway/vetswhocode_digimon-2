3c59a8083313526b1e6c54dcedac59c75ade2ffb
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-19 18:57:05 -0700

    add exercise

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/deal.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/deal.rb
@@ -3,5 +3,14 @@ def deal
   suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
   random_face = faces.sample
   random_suit = suits.sample
-  # call the block here
+  yield(random_face, random_suit)
+end
+
+# Change the deal method so that it calls an associated block, passing it both the randomly-selected face and suit. If you call the method with a block like so...
+# deal { |face, suit| puts "Dealt a #{face} of #{suit}"}
+# that prints out the following
+# Dealt a Jack of Hearts
+
+deal do |face, suit|
+  puts "Dealt an #{face} of #{suit}"
 end
