a74cdc3e5f57801f0ad9a345b675fc0834baef69
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-19 19:00:17 -0700

    add exercise

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/deal.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/deal.rb
@@ -3,7 +3,8 @@ def deal
   suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
   random_face = faces.sample
   random_suit = suits.sample
-  yield(random_face, random_suit)
+  score = yield(random_face, random_suit)
+  puts "You scored a #{score}!"
 end
 
 # Change the deal method so that it calls an associated block, passing it both the randomly-selected face and suit. If you call the method with a block like so...
@@ -12,5 +13,9 @@ end
 # Dealt a Jack of Hearts
 
 deal do |face, suit|
-  puts "Dealt an #{face} of #{suit}"
+  puts "Dealt a #{face} of #{suit}"
+  score = face.length + suit.length
 end
+
+
+# Once you've got the dealing part working, change the block so that it returns a calculated score based on the dealt card. To calculate the score, add the number of characters in the face and the number of characters in the suit.
