926a85e3016095b274e44e75ba928647aa25361f
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-19 18:51:17 -0700

    add exercise

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/iterator.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/iterator.rb
@@ -1,9 +1,9 @@
 #Start on a cheery note by writing a three_times method that takes a block and calls it (wait for it) three times! You want to be able to call the method with an associated block, like so:
 
 def three_times
-  yield(1)
-  yield(2)
-  yield(3)
+  1.upto(3) do |num|
+    yield(num)
+  end
 end
 
 three_times do
@@ -20,3 +20,5 @@ three_times do |number|
   puts "#{number} pushup"
   puts "#{number} chinup"
 end
+
+# If you're in the groove, try changing the method to use the upto method to iterate from 1 to 3. Each time through the loop, call the block and pass the current iteration count as a parameter to the block.
