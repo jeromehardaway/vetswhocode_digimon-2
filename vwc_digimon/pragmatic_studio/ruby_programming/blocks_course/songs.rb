7abe4aa9e685ee2cb3157a66cddc66b1d6b8f83a
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-20 14:56:00 -0700

    add each method

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/songs.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/songs.rb
@@ -2,13 +2,13 @@ class Song
   attr_reader :song_name, :artist, :duration
 
   def initialize(song_name, artist, duration)
-    @song_name = name
+    @song_name = song_name
     @artist = artist
     @duration = duration
   end
 
   def play
-    puts "Playing '#{song_name}' by #{artist} (#{durations} mins)"
+    puts "Playing '#{song_name}' by #{artist} (#{duration} mins)"
   end
 end
 
@@ -21,13 +21,22 @@ class Playlist
   def add_song(song)
     @songs << song
   end
+
+  def each
+    @songs.each { |song| yield song }
+  end
 end
 
-song1 = Song.new("Nobody", "Danelle & Salda", 3:34)
-song2 = Song.new("Tokyo Drift", "Mallrat", 2:51)
-song3 = Song.new("Fuck Apologies", "JoJo", 3:25)
+# create new song objects
+song1 = Song.new("Nobody", "Danelle & Salda", 4)
+song2 = Song.new("Tokyo Drift", "Mallrat", 3)
+song3 = Song.new("Fuck Apologies", "JoJo", 3)
 
+# create new Playlist object and add songs to playlist
 playlist1 = Playlist.new("My Favs")
 playlist1.add_song(song1)
 playlist1.add_song(song2)
 playlist1.add_song(song3)
+
+# define an each method to run the code below
+playlist1.each { |song| song.play }
