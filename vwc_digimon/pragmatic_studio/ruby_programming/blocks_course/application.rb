e4dcbda9accc5d8799a1cb22209e13bb0fce69dc
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-21 12:47:35 -0700

    add file application.rb

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/application.rb
@@ -0,0 +1,24 @@
+class Application
+  attr_accessor :environment
+
+  def initialize
+    @environment = :development
+  end
+
+  def connect_to_database
+    puts "Connecting to #{@environment} database..."
+  end
+
+  def handle_request
+    puts "Handling #{@environment} request..."
+  end
+
+  def write_to_log
+    puts "Writing to #{@environment} log file..."
+  end
+end
+
+app = Application.new
+app.connect_to_database
+app.handle_request
+app.write_to_log
