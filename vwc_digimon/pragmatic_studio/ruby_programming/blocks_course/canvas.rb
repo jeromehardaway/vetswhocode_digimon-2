632c52bce27939b2b6071b43707796ecab1219f7
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-21 17:41:52 -0700

    complete exercises

--- vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/canvas.rb
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/canvas.rb
@@ -1,4 +1,4 @@
-
+# add class Canvas
 class Canvas
   attr_accessor :width, :height, :color
 
@@ -6,6 +6,7 @@ class Canvas
     @width = 100
     @height = 100
     @color = :black
+    yield self if block_given?
   end
 
   def draw_rect(x, y, width, height)
@@ -16,3 +17,19 @@ class Canvas
     "#{@width}x#{@height} #{@color} canvas"
   end
 end
+
+# create a default canvas objects
+# canvas = Canvas.new
+# canvas.width = 250
+# canvas.height = 500
+# canvas.color = :blue
+# puts canvas
+
+# Make sure a Canvas object can be initialized with or without a block
+canvas = Canvas.new do |c|
+ c.width = 800
+ c.height = 600
+ c.color = :green
+end
+
+puts canvas
