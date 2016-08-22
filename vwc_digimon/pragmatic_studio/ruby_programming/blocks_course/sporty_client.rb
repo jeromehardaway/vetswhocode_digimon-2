37c63983766bcb18674e9e8e6d0a8db3195de324
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-21 18:24:34 -0700

    complete exercise

---
+++ vwc_digimon/pragmatic_studio/ruby_programming/blocks_course/sporty_client.rb
@@ -0,0 +1,55 @@
+class SportyClient
+  def post(message)
+    raise "Not signed in!" unless @user
+    # post message to remote service
+    puts "#{@user} posted message: #{message}"
+  end
+
+  def timeline
+    raise "Not signed in!" unless @user
+    # get timeline from remote service
+    puts "#{@user} fetched timeline..."
+  end
+
+  def sign_in(user)
+    # sign in to remote service
+    @user = user
+    puts "#{user} signed in."
+  end
+
+  def sign_out(user)
+    # sign out of remote service
+    puts "#{user} signed out!"
+  end
+
+  def as_signed_in_user(user)
+    sign_in(user)
+    begin
+      yield
+    ensure
+      sign_out(user)
+    end
+  end
+end
+
+client = SportyClient.new
+
+# client.sign_in("broncos_fan")
+# client.post("Ready for the new season...")
+# client.post("Broncos are going all the way!")
+# client.timeline
+# client.sign_out("broncos_fan")
+
+client.as_signed_in_user("broncos_fan") do
+  client.post("Ready for the new season...")
+  client.post("Broncos are going all the way!")
+  client.timeline
+end
+
+# Test that the user is always signed out as the last step, regardless of whether the block completes successfully or raises an exception. For example, try the following code:
+client.as_signed_in_user("Seahawks_12th_man") do
+  client.post("Go Hawks!")
+  raise "You're a Hawks fan!"
+end
+
+# user was signed out successfully
