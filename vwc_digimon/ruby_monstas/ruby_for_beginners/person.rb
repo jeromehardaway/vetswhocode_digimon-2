7659c4c20056b963c924e5dc17f75e2c8622de03
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-25 16:26:53 -0700

    add exercise

--- vwc_digimon/ruby_monstas/ruby_for_beginners/person.rb
+++ vwc_digimon/ruby_monstas/ruby_for_beginners/person.rb
@@ -1,4 +1,19 @@
 class Person
   def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def password=(password)
+    @password = password
   end
 end
+
+person = Person.new("Ellie")
+puts person.name
+
+person.password = "Monkey"
+p person
