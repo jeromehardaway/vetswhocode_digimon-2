3580158b83e334cf3a7c52f5c78b41a09c376327
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-08-27 13:26:27 -0700

    add exercise

---
+++ vwc_digimon/ruby_monstas/ruby_for_beginners/arrays_2-5.rb
@@ -0,0 +1,17 @@
+# change code to return capitalized strings
+
+words = %w(one two three four five)
+words = words.select.with_index { |word, index| index.even? }
+words = words.map { |word| word.capitalize }
+
+# alternative methods
+# words = words.select.with_index { |word, ix| ix.even? }
+# words.delete("two")
+# words.delete("four")
+
+words = words.map.with_index do |word, index|
+  symbol = "<3" * (index * 2 + 1)
+  "#{word} #{symbol}"
+end
+
+puts words.join("\n")
